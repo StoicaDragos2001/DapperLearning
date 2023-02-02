c3321f3e293084cbbf19275cdaf7cfa61006c7f3
StoicaDragos2001 <V3790542@L3700100793>
2023-02-02 15:53:37 +0200

    Add project files.

---
+++ DapperLearning/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperLearning
+{
+    internal class Message
+    {
+        public Guid Id { get; set; }
+        public String Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ DapperLearning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DapperLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DapperLearning.Properties.Settings.SQLDatabase"].ConnectionString);
+
+            using (connection)
+            {
+                connection.Open();
+                Queries.ExposeUsers(connection);
+                Queries.GetPostsOfUser("hfulham0@mtv.com", connection);
+                Queries.InsertUser("Brunhilde", "Paramore", "556-383-3458", "bparamore0@jigsy.com", connection);
+                Queries.ExposeUsers(connection);
+            }
+        }
+    }
+}
