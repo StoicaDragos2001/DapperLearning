3aaed642a5096d640773420b683f703f3038c767
StoicaDragos2001 <V3790542@L3700100793>
2023-02-03 10:46:35 +0200

    Insert Exceptio

---
+++ DapperLearning/Exceptions/UserAlreadyInDatabaseException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DapperLearning.Exceptions
+{
+    internal class UserAlreadyInDatabaseException : Exception
+    {
+        public UserAlreadyInDatabaseException(string message) : base(message)
+        {
+
+        }
+    }
+}
